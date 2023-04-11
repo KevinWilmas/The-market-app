96d9e5f5e095b9f431d17d3659798169e4cd97a3
kevinwilmas <kevinwilmas@outlook.com>
2023-04-10 19:09:28 -0500

    Getters and the cart updated

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,3 +1,4 @@
+import { groupBy } from "lodash";
 import { defineStore } from "pinia";
 
 export const useCartStore = defineStore("CartStore", {
@@ -6,6 +7,13 @@ export const useCartStore = defineStore("CartStore", {
       items: [],
     };
   },
+  getters: {
+    count: (state) => state.items.length,
+    isEmpty: (state) => state.count === 0,
+    grouped: (state) => groupBy(state.items, (item) => item.name),
+    groupCount: (state) => (name) => state.grouped[name].length,
+  },
+
   actions: {
     addItems(count, item) {
       count = parseInt(count);

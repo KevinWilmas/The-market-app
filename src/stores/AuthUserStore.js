1ac873a5783092efa990e2b5b3baca228c7f8a24
kevinwilmas <kevinwilmas@outlook.com>
2023-04-25 20:44:55 -0500

    Built project for deployment

--- src/stores/AuthUserStore.js
+++ src/stores/AuthUserStore.js
@@ -3,7 +3,8 @@ import { defineStore } from "pinia";
 export const useAuthUserStore = defineStore("AuthUserStore", {
   state: () => {
     return {
-      username: "kevinwilmas", // In real life will be imported dynamically
+      username: "kevinwilmas",
+      // In real life will be imported dynamically
     };
   },
 });
